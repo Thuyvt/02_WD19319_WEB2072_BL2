1a331e6993464f0774b98c76aabcd6e767441c5d
thuyvt <thuyvt1005@gmail.com>
2024-10-30 14:11:50 +0700

    first commit

---
+++ src/Day02_App.vue
@@ -0,0 +1,70 @@
+<!--C1: OPTION API -->
+<!-- <script>
+  export default {
+	// khai báo biến
+    data() {
+        return {
+            count: 0
+        }
+    },
+    // khai báo hàm
+	  methods: {
+		    increament() {
+            this.count++;
+        }
+    }
+  }
+</script> -->
+<!-- C2: COMPOSITION API -->
+<script setup>
+    import { ref, reactive} from "vue";
+    // 1. Dùng ref với kiểu dữ liệu nguyên thủy
+    const count = ref(0);
+    const studentName = ref('Vũ Thị Thúy');
+    const studentAge = 18;
+    const status = ref(true);
+
+    // 2. Dùng reactive với dữ liệu dạng đối tượng
+    const studentInfor = reactive({
+        name: 'Nguyễn Văn Lương',
+        age: 18,
+        status: false
+    });
+
+    // Có 2 cú pháp viết hàm
+    const increament = () => {
+        count.value++;
+    }
+
+    function increamentCount() {
+        count.value++;
+    }
+
+</script>
+<template>
+    <!-- OPTION API -->
+    <!-- Count: {{ count }}
+    <button @click="increament">Tăng số lượng</button> -->
+    <!-- COMPOSITION API -->
+    Count: {{ count }}
+    <button @click="increament" class="btn btn-primary">Tăng số lượng 1</button>
+    Count: {{ count }}
+    <button @click="increamentCount" class="btn btn-warning"> Tăng số lượng 2</button>
+    <h3>Thông tin sinh viên 1</h3>
+    <ul class="list-group">
+        <!-- Hiển thị chuỗi -->
+        <li class="list-group-item">Tên sinh viên: {{ studentName }}</li>
+        <!-- Hiển thị số -->
+        <li class="list-group-item">Tuổi: {{ studentAge }}</li>
+        <li class="list-group-item">Trạng thái: {{ status ? 'Đang học' : 'Đã nghỉ'}}</li>
+    </ul>
+    <h3>Thông tin sinh viên 2</h3>
+    <ul class="list-group">
+        <!-- Hiển thị chuỗi -->
+        <li class="list-group-item">Tên sinh viên: {{ studentInfor.name }}</li>
+        <!-- Hiển thị số -->
+        <li class="list-group-item">Tuổi: {{ studentInfor.age }}</li>
+        <li class="list-group-item">Trạng thái: {{ studentInfor.status ? 'Đang học' : 'Đã nghỉ'}}</li>
+    </ul>
+
+</template>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+// khai báo thư viện bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// import App from './App.vue'
+// import App from './Day01_App.vue'
+import App from './Day02_App.vue'
+
+createApp(App).mount('#app')
