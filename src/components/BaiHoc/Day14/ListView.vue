b5f3dcc6df1ad994707ba54161721abaf6b126fc
thuyvt <thuyvt1005@gmail.com>
2024-11-29 12:58:10 +0700

    a

--- src/components/BaiHoc/Day14/ListView.vue
+++ src/components/BaiHoc/Day14/ListView.vue
@@ -1,16 +1,31 @@
 <script setup>
+    import ConfirmDelete from "./Modal/ConfirmDelete.vue";
     import { ref , onMounted } from "vue";
     // Khai báo kết nối
     import instanceAxios from '@/ultis/configAxios';
     // Khai báo danh sách books
     const books = ref();
-// Lấy danh sách books
-const getListBooks = async() => {
+    // Tạo biến ẩn hiện modal
+    const isShowModal = ref(false);
+    // Biến lấy giá trị đối tượng được chọn
+    const bookSelected = ref();
+    // Lấy danh sách books
+    const getListBooks = async() => {
         const response = await instanceAxios.get('books');
         if (response && response.data) {
             books.value = response.data;
         }
         console.log(books.value);
+    }
+
+    // hàm bật hiện modal
+    const handleToggleModal = async(book) => {
+      isShowModal.value = true;
+      bookSelected.value = book;
+    }
+    // hàm đóng modal
+    const handleCloseModel = () => {
+      isShowModal.value = false;
     }
     // Gọi hàm lấy danh sách
     onMounted(() => {
@@ -48,10 +63,16 @@ const getListBooks = async() => {
                       :to="{ name: 'update', params: { id: book.id } }"
                       class="btn btn-success me-3"> Cập nhật
                     </RouterLink>
-                    <button class="btn btn-danger" @click="onClickDeleteBook(book.id)">Xóa</button>
-
+                    <button class="btn btn-danger" @click="handleToggleModal(book)">Xóa</button>
                   </td>
             </tr>
         </tbody>
     </table>
+    <!-- Dùng props lấy giá trị từ cha vào con,
+     : truyền giá trị từ cha vào con -->
+    <ConfirmDelete v-if="isShowModal"
+    :isShowModal="isShowModal"
+    :handleCloseModel="handleCloseModel"
+    :bookSelected="bookSelected">
+    </ConfirmDelete>
 </template>
--- src/components/BaiHoc/Day14/UpdateView.vue
+++ src/components/BaiHoc/Day14/UpdateView.vue
@@ -1,39 +1,70 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import instanceAxios from "@/ultis/configAxios";
+// Khai báo hàm để lấy thông tin tham số trên url
+import { useRoute } from "vue-router";
+
+const book = ref({
+  id: "",
+  name: "",
+  author: "",
+  price: 0,
+  quantity: 0,
+});
+// biến hứng giá trị tham số trên url
+console.log(useRoute().params);
+const bookId = useRoute().params.id;
+
+// Hàm lấy thông tin đối tượng
+const getDetail = async() => {
+  // call api lấy thoogn tin chi tiết
+  const response = await instanceAxios.get(`/books/${bookId}`);
+  if (response) {
+    book.value = response.data
+  }
+}
+
+onMounted(() => {
+  // Gọi hàm lấy thông tin chi tiết đối tượng
+  getDetail();
+})
+</script>
+
 <template>
   <h3>Cập nhật</h3>
   <form>
-        <div class="mt-3">
-            <span class="form-label">ID:</span>
-            <input class="form-control" type="text" v-model.trim="book.id" disabled>
-        </div>
+    <div class="mt-3">
+      <span class="form-label">ID:</span>
+      <input class="form-control" type="text" v-model.trim="book.id" disabled />
+    </div>
 
-        <div class="mt-3">
-            <span class="form-label">Tên:</span>
-            <input class="form-control" type="text" v-model.trim="book.name">
-        </div>
+    <div class="mt-3">
+      <span class="form-label">Tên:</span>
+      <input class="form-control" type="text" v-model.trim="book.name" />
+    </div>
 
-        <div class="mt-3">
-            <span class="form-label">Tác giả:</span>
-            <select class="form-control" v-model="book.author">
-                <option value="">- Lựa chọn -</option>
-                <option value="Nguyễn Nhật Ánh">Nguyễn Nhật Ánh</option>
-                <option value="Vũ Trọng Phụng">Vũ Trọng Phụng</option>
-                <option value="Nam Cao">Nam Cao</option>
-            </select>
-        </div>
+    <div class="mt-3">
+      <span class="form-label">Tác giả:</span>
+      <select class="form-control" v-model="book.author">
+        <option value="">- Lựa chọn -</option>
+        <option value="Nguyễn Nhật Ánh">Nguyễn Nhật Ánh</option>
+        <option value="Vũ Trọng Phụng">Vũ Trọng Phụng</option>
+        <option value="Nam Cao">Nam Cao</option>
+      </select>
+    </div>
 
-        <div class="mt-3">
-            <span class="form-label">Giá:</span>
-            <input class="form-control" type="text" v-model.number="book.price">
+    <div class="mt-3">
+      <span class="form-label">Giá:</span>
+      <input class="form-control" type="text" v-model.number="book.price" />
+    </div>
+    <div class="mt-3">
+      <span class="form-label">Số lượng:</span>
+      <input class="form-control" type="text" v-model.number="book.quantity" />
+    </div>
 
-        </div>
-        <div class="mt-3">
-            <span class="form-label">Số lượng:</span>
-            <input class="form-control" type="text" v-model.number="book.quantity">
-        </div>
-
-        <div class="mt-3 text-center">
-            <a class="btn btn-success me-3" @click="onClickCreate">Tạo Mới</a>
-            <a class="btn btn-info" @click="onClickUpdate(book.id)">Cập nhật</a>
-        </div>
-    </form>
+    <div class="mt-3 text-center">
+      <a class="btn btn-success me-3" @click="onClickCreate">Tạo Mới</a>
+      <a class="btn btn-info" @click="onClickUpdate(book.id)">Cập nhật</a>
+    </div>
+  </form>
 </template>
